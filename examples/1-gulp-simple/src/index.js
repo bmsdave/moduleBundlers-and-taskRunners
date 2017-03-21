3877ea1e3346e993be9afed754f47265c2f245c3
Vadim Gorbachev <bmsdave@gmail.com>
2017-03-21 13:32:51 +0300

    remove dest

--- examples/1-gulp-simple/src/index.js
+++ examples/1-gulp-simple/src/index.js
@@ -3,14 +3,11 @@
 var app, intervalID;
 
 function ready() {
-
-	app = document.querySelector('.app');
-
-	function now() {
-		app.childNodes[0].textContent = (new Date()).toString();
-	};
-
-	intervalID = window.setInterval(now, 500)
+    app = document.querySelector('.app');
+    function now() {
+        app.childNodes[0].textContent = (new Date()).toString();
+    };
+    intervalID = window.setInterval(now, 500)
 
 }
 
